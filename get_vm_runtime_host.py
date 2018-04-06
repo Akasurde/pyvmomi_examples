78d2a8640b1b14b34d01c24868480fe4b9d8f765
Abhijeet Kasurde <akasurde@redhat.com>
2018-04-06 17:11:53 +0530

    Get runtime hostsystem
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ get_vm_runtime_host.py
@@ -0,0 +1,44 @@
+from pyVim.connect import SmartConnect, Disconnect
+import atexit
+from pyVmomi import vim
+import os
+
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.verify_mode = ssl.CERT_NONE
+
+hostname = ''
+username = ''
+password = ''
+vm_name = ''
+
+si = SmartConnect(host=hostname, user=username, pwd=password, port=443, sslContext=context)
+atexit.register(Disconnect, si)
+content = si.RetrieveContent()
+
+def get_obj(content, vimtype, name=None):
+    """
+    Return an object by name, if name is None the
+    first found object is returned
+    """
+    obj = None
+    if name is None:
+        obj = []
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True)
+    for c in container.view:
+        if name:
+            if c.name == name:
+                obj = c
+                break
+        else:
+            obj.append(c)
+            break
+
+    container.Destroy()
+    return obj
+
+
+vm = get_obj(content, [vim.VirtualMachine], vm_name)
+print(vm.summary.runtime.host)
+
